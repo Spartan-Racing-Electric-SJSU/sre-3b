d7cd9278711db564f85cc582b23709489d538395
Rusty.P <rusty@guysmily.com>
2016-06-11 06:00:27 -0700

    Moved BMS byte swapping stuff to mathFunctions.c

--- mathFunctions.c
+++ mathFunctions.c
@@ -1,4 +1,4 @@
-
+#include <stdarg.h>
 #include <math.h>
 #include "mathFunctions.h"
 #include "IO_Driver.h"
@@ -17,7 +17,7 @@
 float4 getPercent(float4 value, float4 start, float4 end, bool zeroToOneOnly)
 {
     float4 retVal;
-
+    
     //Flip values and start at 0
     float4 min = (start < end) ? start : end;
     float4 max = (start < end) ? end : start;
@@ -47,7 +47,13 @@ float4 getPercent(float4 value, float4 start, float4 end, bool zeroToOneOnly)
 // A utility function to get maximum of two integers
 ubyte2 max(ubyte2 a, ubyte2 b)
 {
-	return (a > b) ? a : b;
+    return (a > b) ? a : b;
+}
+
+// A utility function to get maximum of two integers
+ubyte2 min(ubyte2 a, ubyte2 b)
+{
+    return (a < b) ? a : b;
 }
 
 
@@ -73,7 +79,6 @@ ubyte2 max(ubyte2 a, ubyte2 b)
  ***************************************************************************/
 bool blink(ubyte4* clock, ubyte2 highPeriod)
 {
-
     //time passed since the start of the blinks divided by the period to get
     //count.
     //count % 2 gets the current state the blink should be in 0 or <0
@@ -83,3 +88,44 @@ bool blink(ubyte4* clock, ubyte2 highPeriod)
 
     return !( count / highPeriod ) % 2;
 }
+
+
+
+
+
+//byte swapping functions used by BMS
+
+ubyte1 swap_uint8(ubyte1 val)
+{
+    return (val << 4) | (val >> 4);
+}
+
+sbyte1 swap_int8(sbyte1 val)
+{
+    return (val << 4) | (val >> 4);
+}
+ubyte2 swap_uint16(ubyte2 val)
+{
+    return (val << 8) | (val >> 8);
+}
+
+//! Byte swap short
+sbyte2 swap_int16(sbyte2 val)
+{
+    return (val << 8) | ((val >> 8) & 0xFF);
+}
+
+//! Byte swap unsigned int
+ubyte4 swap_uint32(ubyte4 val)
+{
+    val = ((val << 8) & 0xFF00FF00) | ((val >> 8) & 0xFF00FF);
+    return (val << 16) | (val >> 16);
+}
+
+//! Byte swap int
+sbyte4 swap_int32(sbyte4 val)
+{
+    val = ((val << 8) & 0xFF00FF00) | ((val >> 8) & 0xFF00FF);
+    return (val << 16) | ((val >> 16) & 0xFFFF);
+}
+
--- mathFunctions.h
+++ mathFunctions.h
@@ -18,4 +18,14 @@ float4 getPercent(float4 value, float4 start, float4 end, bool zeroToOneOnly);
 ubyte2 max(ubyte2 a, ubyte2 b);
 
 
+/*
+*  Functions for endian conversion
+*/
+ubyte1 swap_uint8(ubyte1 val);
+sbyte1 swap_int8(sbyte1 val);
+ubyte2 swap_uint16(ubyte2 val);
+sbyte2 swap_int16(sbyte2 val);
+ubyte4 swap_uint32(ubyte4 val);
+sbyte4 swap_int32(sbyte4 val);
+
 #endif //  _MATHFUNCTIONS_H
