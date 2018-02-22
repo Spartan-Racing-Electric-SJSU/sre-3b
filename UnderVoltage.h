05a5ef3e1c09380e7a8e7cdb82534e67c4daf479
Francisco-Ibarra07 <francisco.ibarra@sjsu.edu>
2018-02-21 18:56:22 -0800

    Update UnderVoltage.h

--- UnderVoltage.h
+++ UnderVoltage.h
@@ -1,1 +1,38 @@
+// UnderVoltage Protection handler
+#ifdef _UNDERVOLTAGE_
+#define _UNDERVOLTAGE_ 
+
+#include <stdio.h>
+
+
+char* errorMessage; //This will be used for error/fault messages
+
+
+struct batteryFaults {
+
+	//For battery levels
+	const int minimumThresshold; //This will be 12.8
+	float currentBatteryLevel;
+	float warningBatteryConstant; //GetBatteryValue() - (0.5)
+
+	//For temperature of the battery
+	const int maxTempThresshold;
+	float batteryTemperature;
+	float warningTempConstant;
+
+} LVBattery;
+
+struct LVComponentFaults {
+
+	float current;
+
+
+
+
+} motorcontroller ;
+
+
+struct _MotorController LVmotorController;
+
+
 
