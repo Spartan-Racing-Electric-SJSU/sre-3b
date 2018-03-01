ecd0d9e206ab47d042f3a548b20eff8ce5890931
Francisco Ibarra <francisco.ibarra@sjsu.edu>
2018-03-01 14:13:02 -0800

    Added two more faults

--- underVoltageProtection.h
+++ underVoltageProtection.h
@@ -4,21 +4,22 @@
 
 #include <stdio.h>
 
-char* errorMessage; //the char pointer is a global variable that is used for any error message related to LV systems
 
 typedef struct batteryFaults{
 
-	float currentBatteryLevel; //gets information from the LV battery about it's current battery life (look at how other sensors extract voltage data from the LV battery)
-	float warningBatteryConstant // = batteryPercentage - 0.5; get voltage value from the LV battery and subtract it by a placeholder constant value
-	int minBatteryThreshold; //a constant for the minimum battery threshold
+	float4 currentBatteryLevel; //gets information from the LV battery about it's current battery life (look at how other sensors extract voltage data from the LV battery)
+	float4 warningBatteryConstant // = batteryPercentage - 0.5; get voltage value from the LV battery and subtract it by a placeholder constant value
+	int    minBatteryThreshold; //a constant for the minimum battery threshold
+	int    maxBatteryThreshold; 
 
-	float batteryTemperature; //maximum threshold for the battery temperature
-	float warningTempConstant; //= batteryTemperature + 0.5;
-	int maxTempThreshold
+	float4 batteryTemperature; //maximum threshold for the battery temperature
+	float4 warningTempConstant; //= batteryTemperature + 0.5;
+	int    maxTempThreshold;
+	int    minTempThreshold;
 
-} lvBattery;
+} LVBattery;
 
-typedef struct LVComponentFaults{
+typedef struct LVComponent{
 
 	float current; //gets feedback from the LV components on what its current is
 	float voltage; //gets feedback from the LV components on what its voltage is
@@ -37,33 +38,20 @@ struct _MotorController lvMotorController;
 //Functions
 /*Acessors*/
 
-float getLV_Voltage()
-float getLV_Temperature() 
+float4 getLV_Voltage();
+float4 getLV_Temperature(); 
 
-float motorcontroller_Voltage()
-float motorcontroller_Current()
-float motorcontroller_Temperature()
+float4 motorcontroller_Voltage();
+float4 motorcontroller_Current();
+float4 motorcontroller_Temperature();
 
-float cooling_Voltage()
-float cooling_Temperature()
+float4 cooling_Voltage()
+float4 cooling_Temperature()
 
-
-
-float dcu_Voltage()
 
 float brakePressure_Voltage()
 float wheelSpeeds_Voltage();
-																					/*	LVBattery.warningBatteryConstant; 
-																						LVBattery.minBatteryThreshold = 12.8;
-
-																						LVBattery.warningTempConstant = 90;
-																						LVBattery.maxTempThreshold;
-
-																						motorcontroller. */
-
-
-																						//return lvMotorController.DC_Voltage;
-
+																		
 
 /*Mutators*/
 void setConstants()
