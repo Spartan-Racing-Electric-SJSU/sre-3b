b0b67928d159849683621daec6b93d90872764ee
WilsonHChung <33712376+WilsonHChung@users.noreply.github.com>
2018-03-07 18:27:18 -0800

    Initialization and Deinitialization of Serial Messaging

---
+++ underVoltageProtection.c
@@ -0,0 +1,112 @@
+#include "underVoltageProtection.h"
+#include "IO_CAN.h"
+#include "motorController.h"
+#include "bms.h"
+
+
+/* Global variables */
+
+/*In order for the ADC value to keep being read, this value must be true.
+* Initially set to FALSE, but it initialization clears well, this variable will be set to TRUE
+* If at any point anything goes wrong with the ADC pin or channel, this will be set to FALSE and the program will not read any value
+*/
+bool ADC_Channel_OK = FALSE; 
+
+/*In order for the PWM value to keep being read, this value must be true.
+* Initially set to FALSE, but it initialization clears well, this variable will be set to TRUE
+* If at any point anything goes wrong with the PWM pin or channel, this will be set to FALSE and the program will not read any value
+*/
+bool PWM_Channel_OK = FALSE; 
+
+IO_ErrorType ADC_Pin_Status = NULL; //Stores the current IO_ErrorType of the ADC pin
+IO_ErrorType PWM_Pin_Status = NULL; //Stores the current IO_ErrorType of the PWM pin
+
+LVBattery.warningBatteryConstant=0;
+LVBattery.minBatteryThreshold= 12.8; 
+LVBattery.maxBatteryThreshold= 14.8;
+
+LVBattery.batteryTemperature= NULL;
+LVBattery.warningTempConstant= NULL;
+LVBattery.maxTempThreshold= NULL;
+LVBattery.minTempThreshold= NULL;
+
+
+ /** FUNCTION DEFINITIONS **/
+bool ADC_Channel_OK( IO_ErrorType status ) {
+	switch(status) 
+	{
+		case IO_E_OK:
+			ADC_Channel_OK_Value = TRUE;
+			break;
+		case IO_E_INVALID_CHANNEL_ID:
+			ADC_Channel_OK_Value = FALSE;
+			break;
+		case IO_E_NULL_POINTER:
+			ADC_Channel_OK_Value = FALSE;
+			break;
+		case IO_E_FET_PROTECTION:
+			ADC_Channel_OK_Value = FALSE;
+			break;
+		case IO_E_UNKNOWN:
+			ADC_Channel_OK_Value = FALSE;
+			break;
+		case IO_E_CHANNEL_NOT_CONFIGURED:
+			ADC_Channel_OK_Value = FALSE;
+			break;
+		default:
+			ADC_Channel_OK_Value = FALSE;
+			break;
+	}
+	return ADC_Channel_OK_Value;
+}
+
+bool PWM_Channel_OK( IO_ErrorType status ) {
+	switch(status) 
+	{
+		case IO_E_OK:
+			PWM_Channel_OK = TRUE;
+			break;
+		case IO_E_INVALID_CHANNEL_ID:
+			PWM_Channel_OK = FALSE;
+			break;
+		case IO_E_NULL_POINTER:
+			PWM_Channel_OK = FALSE;
+			break;
+		case IO_E_FET_PROTECTION:
+			PWM_Channel_OK = FALSE;
+			break;
+		case IO_E_UNKNOWN:
+			PWM_Channel_OK = FALSE;
+			break;
+		case IO_E_CHANNEL_NOT_CONFIGURED:
+			PWM_Channel_OK = FALSE;
+			break;
+		default:
+			PWM_Channel_OK = FALSE;
+			break;
+	}
+	return PWM_Channel_OK;
+}
+
+float4 getLV_Voltage( void ){ //returns the voltage of the LV Battery 
+	return LVBattery.currentBatteryLevel;
+}
+
+float4 getLV_Temperature( void ) { //returns the temperature of the LV Battery
+	return LVBattery.batteryTemperature;
+}
+
+float4 motorcontroller_Voltage( void ){ //returns the voltage of the motor controller
+	return (me->DC_Voltage);
+}
+
+float4 motorcontroller_Current( void ){ //returns the current of the motor controller
+	return (me->DC_Current);
+}
+
+float4 motorcontroller_Temperature( void ){ //returns the temperature of the motor controller
+	return MCM_getTemp(mcm0); 
+}
+void cooling_Regulate( CoolingSystem* me, sbyte2 motorControllerTemp ){ //regulates cooling based on undervoltage and temperature of the motors and the motor controller
+	
+}
---
+++ underVoltageProtectionMain.c
@@ -0,0 +1,116 @@
+//This will be the implementation file of the under voltage code
+#include "underVoltageProtection.c"
+
+APDB appl_db =
+{ 0 /* ubyte4 versionAPDB */
+, {0} /* BL_T_DATE flashDate */
+/* BL_T_DATE buildDate */
+, { (ubyte4) (((((ubyte4) RTS_TTC_FLASH_DATE_YEAR) & 0x0FFF) << 0) |
+((((ubyte4) RTS_TTC_FLASH_DATE_MONTH) & 0x0F) << 12) |
+((((ubyte4) RTS_TTC_FLASH_DATE_DAY) & 0x1F) << 16) |
+((((ubyte4) RTS_TTC_FLASH_DATE_HOUR) & 0x1F) << 21) |
+((((ubyte4) RTS_TTC_FLASH_DATE_MINUTE) & 0x3F) << 26)) }
+, 0 /* ubyte4 nodeType */
+, 0 /* ubyte4 startAddress */
+, 0 /* ubyte4 codeSize */
+, 0 /* ubyte4 legacyAppCRC */
+, 0 /* ubyte4 appCRC */
+, 1 /* ubyte1 nodeNr */
+, 0 /* ubyte4 CRCInit */
+, 0 /* ubyte4 flags */
+, 0 /* ubyte4 hook1 */
+, 0 /* ubyte4 hook2 */
+, 0 /* ubyte4 hook3 */
+, APPL_START /* ubyte4 mainAddress */
+, {0, 1} /* BL_T_CAN_ID canDownloadID */
+, {0, 2} /* BL_T_CAN_ID canUploadID */
+, 0 /* ubyte4 legacyHeaderCRC */
+, 0 /* ubyte4 version */
+, 500 /* ubyte2 canBaudrate */
+, 0 /* ubyte1 canChannel */
+, {0} /* ubyte1 reserved[8*4] */
+, 0 /* ubyte4 headerCRC */
+};
+
+void main(void)
+{
+	//Initialize the driver
+	IO_Driver_Init( NULL );
+	IO_DI_DeInit( IO_ADC_5V_02 ); //TODO: change the IO_DI_DeInit to the right input and check with tim to makes sure all arguments are correct
+	
+	/* ADC CHANNEL INITIALIZATION */
+	IO_ErrorType ADC_Pin_Status = IO_ADC_ChannelInit (IO_ADC_5v_04, //adc channel
+	                    IO_ADC_ABSOLUTE, //type. Absolute means normal voltage input
+	                    IO_ADC_RANGE_5V, //range. IO_ADC_RANGE_5V means 0-5V input
+	                    IO_ADC_PU_10K, // pupd. IO_ADC_PU_10K means pull up resistor
+	                    IO_ADC_SENSOR_SUPPLY_0, //sensor supply. Unclear on this one
+	                    IO_ADC_SAFETY_CONF const * const safety_conf ); // Safety_conf. also unclear on this one
+
+	 /* CAN CHANNEL INITIALIZATION */
+	 IO_ErrorType IO_CAN_Init( ubyte1 channel
+                         , ubyte2 baudrate
+                         , ubyte1 tseg1
+                         , ubyte1 tseg2
+                         , ubyte1 sjw );
+
+	 /* SERIAL INITIALIZATION */
+	  IO_ErrorType IO_UART_Init( ubyte1 channel
+                          , ubyte4 baudrate
+                          , ubyte1 dbits
+                          , ubyte1 par
+                          , ubyte1 sbits);
+	
+	/* POSSIBLE RETURN VALUES FOR ADC_Pin_Status 
+ 	* \return IO_ErrorType:
+ 	* \retval IO_E_OK                          everything fine
+ 	* \retval IO_E_INVALID_CHANNEL_ID          the channel id does not exist
+ 	* \retval IO_E_INVALID_PARAMETER           parameter is out of range
+ 	* \retval IO_E_CHANNEL_BUSY                the ADC input channel is currently used by another function
+ 	* \retval IO_E_DRV_SAFETY_CONF_NOT_CONFIG  Global safety configuration is missing  */
+	IO_ADC_ChannelDeInit( ubyte1 adc_channel );
+
+	/* CAN CHANNEL DENINITIALIZATION */
+	IO_CAN_DeInit( ubyte1 );
+
+	/* SERIAL DEINITIALIZATION */
+	IO_UART_DeInit(ubyte1 channel);
+
+	IO_ErrorType PWM_Pin_Status = IO_PWM_Init( IO_PWM_02, 50, TRUE, bool cur_measurement, ubyte1 cur_channel, bool diag_margin, NULL ); 
+/* POSSIBLE RETURN VALUES FOR PWM_Pin_Status
+	* \return IO_ErrorType:
+	* \retval IO_E_OK                          everything fine
+	* \retval IO_E_CHANNEL_BUSY                the PWM output channel or the timer input channel is
+	*                                          currently used by another function
+	* \retval IO_E_INVALID_CHANNEL_ID          the PWM output channel or the timer input channel
+	*                                          id don't exist
+	* \retval IO_E_INVALID_PARAMETER           a given parameter is out of range
+	* \retval IO_E_DRV_SAFETY_CONF_NOT_CONFIG  Global safety configuration is missing 
+	*/
+    IO_PWM_DeInit( IO_PWM_02 );
+
+	
+	while (1)
+	{
+	
+		IO_RTC_StartTime(&timestamp);
+		
+
+		//While the ADC channel and PWM channel doesn't return an error, the ADC will read the value and PWM write is allowed
+		if( ADC_Channel_OK(ADC_pin_Status) == TRUE && PWM_Channel_OK(PWM_Pin_Status) == TRUE ) 
+		{
+			LVBattery.currentBatteryLevel = IO_ADC_Get(IO_ADC_5v_04, IO_ADC_ABSOLUTE, TRUE); 
+
+				if(LVBattery.currentBatteryLevel - 0.5 < LVBattery.minBatteryThreshold)
+				{
+					IO_PWM_SetDuty(IO_PWM_02, 100, NULL); //100% duty cycle
+				}
+				else { IO_PWM_SetDuty(IO_PWM_02, 0, NULL); } //0% duty cycle
+		}
+			
+
+		IO_Driver_TaskEnd();
+		while (IO_RTC_GetTimeUS(timestamp) < 50000);
+		{
+		}
+	
+}
