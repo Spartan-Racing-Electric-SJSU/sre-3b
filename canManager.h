160f1e04387b90c7682bd143a645485f7a8d91f2
WilsonHChung <33712376+WilsonHChung@users.noreply.github.com>
2018-03-15 14:59:36 -0700

    Initialization of CAN Messaging for UnderVoltage (Note: Only used for UnderVoltage testing)

--- canManager.h
+++ canManager.h
@@ -4,11 +4,8 @@
 #include "IO_Driver.h" 
 #include "IO_CAN.h"
 
-#include "avlTree.h"
-#include "motorController.h"
-#include "bms.h"
-#include "wheelSpeeds.h"
-#include "safety.h"
+/* include underVoltage or any other files here */
+#include "underVoltageProtection.h"
 
 typedef enum { CAN0_HIPRI, CAN1_LOPRI } CanChannel;
 //CAN0: 48 messages per handle (48 read, 48 write)
@@ -25,12 +22,18 @@ CanManager* CanManager_new(ubyte2 can0_busSpeed, ubyte1 can0_read_messageLimit, 
 IO_ErrorType CanManager_send(CanManager* me, CanChannel channel, IO_CAN_DATA_FRAME canMessages[], ubyte1 canMessageCount);
 
 //Reads and distributes can messages to their appropriate subsystem objects so they can updates themselves
-void CanManager_read(CanManager* me, CanChannel channel, MotorController* mcm, BatteryManagementSystem* bms, SafetyChecker* sc);
+void CanManager_read(CanManager* me, CanChannel channel, underVoltage_ *uv);
 
+/* ADC voltage output that helps with indicating the voltage level */
 void canOutput_sendSensorMessages(CanManager* me);
-//void canOutput_sendMCUControl(CanManager* me, MotorController* mcm, bool sendEvenIfNoChanges);
-void canOutput_sendDebugMessage(CanManager* me, TorqueEncoder* tps, BrakePressureSensor* bps, MotorController* mcm, WheelSpeeds* wss, SafetyChecker* sc);
 
+/* Motor Controller Unit CAN Control */
+//void canOutput_sendMCUControl(CanManager* me, MotorController* mcm, bool sendEvenIfNoChanges);
+
+/* Debug messaging for Under Voltage */
+void canOutput_sendDebugMessage(CanManager* me, underVoltage_ *uv);
+
+/* Interprets the status of the CAN channel at the time of invocation */
 ubyte1 CanManager_getReadStatus(CanManager* me, CanChannel channel);
 
 #endif // _CANMANAGER_H is defined
